c8716e9b11d8e341fca77599ede5e48d16d6dbb4
damunro <dmunro@mines.edu>
2025-04-24 20:27:32 -0500

    redid navbar/homepage

---
+++ portfolio-ui/src/app/about/page.tsx
@@ -0,0 +1,16 @@
+import { Card, Link as ChakraLink } from "@chakra-ui/react";
+
+import Link from "next/link";
+
+export default function Projects() {
+	const projects = [
+		{
+			name: "calculator",
+			desc: "Simple calculator app",
+			image: "",
+			link: "/calculator",
+		},
+	];
+
+	return <div>About me</div>;
+}
--- portfolio-ui/src/components/layout/header.tsx
+++ portfolio-ui/src/components/layout/header.tsx
@@ -1,24 +1,17 @@
 import { Link as ChakraLink, HStack } from "@chakra-ui/react";
 
-import Image from "next/image";
+import { FaFish } from "react-icons/fa";
 import Link from "next/link";
 
 export default function Header() {
 	return (
-		<HStack>
-			<Image
-				className="dark:invert"
-				src="/next.svg"
-				alt="Next.js logo"
-				width={180}
-				height={38}
-				priority
-			/>
+		<HStack textStyle="4xl" px="10" py="5" bg="lightblue">
+			<FaFish />
 			<ChakraLink asChild>
 				<Link href="/">Home</Link>
 			</ChakraLink>
-			<ChakraLink>
-				<Link href="/projects">Projects</Link>
+			<ChakraLink asChild>
+				<Link href="/about">About</Link>
 			</ChakraLink>
 		</HStack>
 	);
